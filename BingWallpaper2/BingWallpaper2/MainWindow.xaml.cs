9b3f6a23754534a82277c0c8f55263d331d06021
yinyue <yinxiyue1995@outlook.com>
2019-12-05 17:18:18 +0800

    获取路径

--- BingWallpaper2/BingWallpaper2/MainWindow.xaml.cs
+++ BingWallpaper2/BingWallpaper2/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace BingWallpaper
 {
@@ -29,6 +23,16 @@ namespace BingWallpaper
         private void InitializeUI()
         {
             Title = $"每日必应壁纸2 version_{System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()}";
+        }
+
+        private void btnChoosePath_Click(object sender, RoutedEventArgs e)
+        {
+            var browserDialog = new FolderBrowserDialog();
+            if (browserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string chooseDir = browserDialog.SelectedPath.Trim();
+                tbImageSavePath.Text = chooseDir;
+            }
         }
     }
 }
