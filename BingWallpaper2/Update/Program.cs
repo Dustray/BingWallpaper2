af5a7367d3a28ef7db8d0ca5dac5c4db61b0350f
Dustray <yinxiyue1995@outlook.com>
2019-12-22 12:25:46 +0800

    完善自动更新

--- BingWallpaper2/Update/Program.cs
+++ BingWallpaper2/Update/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace Update
 {
@@ -10,15 +11,21 @@ namespace Update
     {
         static void Main(string[] args)
         {
-            var filePath = Path.GetFullPath(@"Temp\newupdate.ztmp");
-            var unzipPath = Environment.CurrentDirectory;
+            var model = LoadConfig();
+            if (null == model)
+            {
+                return;
+            }
+            var filePath = Path.GetFullPath(model.SourceFile/*@"Temp\newupdate.ztmp"*/);
+            var programPath = Environment.CurrentDirectory;
+            var unzipPath = programPath;
             if (IntPtr.Size == 4)
             {
-                SevenZipExtractor.SetLibraryPath(@"x86\7z.dll");
+                SevenZipExtractor.SetLibraryPath(model.dllx86/*@"x86\7z.dll"*/);
             }
             else
             {
-                SevenZipExtractor.SetLibraryPath(@"x64\7z.dll");
+                SevenZipExtractor.SetLibraryPath(model.dllx64/*@"x64\7z.dll"*/);
             }
             for (int k = 0; k < 3; k++)
             {
@@ -32,17 +39,67 @@ namespace Update
                         {
                             tmp.ExtractFiles(unzipPath, tmp.ArchiveFileData[i].Index);
                         }
+                        try
+                        {
+                            Process m_Process = new Process();
+                            m_Process.StartInfo.FileName = Path.GetFullPath(model.LauncherProgram/*$"BingWallpaper2.exe"*/);
+                            m_Process.Start();
+                            Environment.Exit(0);
+                        }
+                        catch
+                        {
+                            Environment.Exit(0);
+                        }
                     }
-                    Process m_Process = new Process();
-                    m_Process.StartInfo.FileName = Path.GetFullPath($"BingWallpaper2.exe");
-                    m_Process.Start();
-                    Environment.Exit(0);
                 }
-                catch
+                catch (Exception e)
                 {
-
+                    MessageBox.Show("更新失败，未找到更新文件。"+e);
+                    return;
                 }
             }
+        }
+
+        /// <summary>
+        /// 加载指定XML文件并序列化为指定泛型对象
+        /// <para>异步的</para>
+        /// </summary>
+        /// <typeparam name="T">泛型对象</typeparam>
+        /// <param name="xmlFilePath">XML文件路径(绝对路径)</param>
+        /// <returns>加载结果</returns>
+        public static SevenModel LoadConfig()
+        {
+            int counter = 0;
+            var lines = new string[5];
+            string line;
+            try
+            {
+                using (var file = new StreamReader(Path.Combine(@"Config\SevenZipConfig.cfg")))
+                {
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        lines[counter] = line;
+                        counter++;
+                    }
+                    if (counter != 5)
+                    {
+                        MessageBox.Show("配置文件解析失败，请手动下载程序。");
+                        return null;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("更新失败，缺少配置文件，请手动下载程序。");
+                return null;
+            }
+            var m = new SevenModel();
+            m.SourceFile = lines[0];
+            m.TargetFile = lines[1];
+            m.dllx86 = lines[2];
+            m.dllx64 = lines[3];
+            m.LauncherProgram = lines[4];
+            return m;
         }
     }
 }
---
+++ BingWallpaper2/Update/SevenModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Update
+{
+    public class SevenModel
+    {
+        public string SourceFile { get; set; }
+        public string TargetFile { get; set; }
+        public string dllx86 { get; set; }
+        public string dllx64 { get; set; }
+        public string LauncherProgram { get; set; }
+    }
+}
