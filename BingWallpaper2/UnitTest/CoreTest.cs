eb485fc0b57e4354130c6f8cb1593add82e90ab1
Dustray <yinxiyue1995@outlook.com>
2019-12-25 22:11:36 +0800

    单元测试

---
+++ BingWallpaper2/UnitTest/CoreTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BingWallpaper.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class CoreTest
+    {
+        [TestMethod]
+        public void SetWallpaperWeb()
+        {
+            CoreEngine.Current.SetWallpaper(true);
+        }
+
+        [TestMethod]
+        public void SetWallpaperLocal()
+        {
+            CoreEngine.Current.SetWallpaper(false);
+        }
+
+        [TestMethod]
+        public void SetWallpaperWebAsync()
+        {
+            CoreEngine.Current.SetWallpaper(true);
+        }
+
+        [TestMethod]
+        public void SetWallpaperLocalAsync()
+        {
+            CoreEngine.Current.SetWallpaper(false);
+        }
+    }
+}
---
+++ BingWallpaper2/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6af55256-811f-49f5-9565-19f9af491d98")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
